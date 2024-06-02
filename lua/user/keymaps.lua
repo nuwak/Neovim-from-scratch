868f8aa1d7d0be45720bc0d74af57cbdf85282c5
Zer0 <zer0cube@pm.me>
2024-06-02 19:15:49 +0200

    my map

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -34,8 +34,8 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", "$", opts)
+keymap("n", "<S-h>", "^", opts)
 
 -- Insert --
 -- Press jk fast to enter
